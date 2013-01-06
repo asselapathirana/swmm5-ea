7eab1c08947bf09bb5f271767713d09e0b3b14f3
assela.pathirana <assela.pathirana@311b4d62-726b-ab8b-920d-ce57b7db7bff>
2013-01-06 20:55:21 +0000

    done

--- setup.py
+++ setup.py
@@ -3,12 +3,18 @@ from distutils.core import setup
 from itertools import product
 from swmm5ea import swmm_ea_controller as sc
 
+with open("README.txt","r") as f:
+    README=f.read()
+
 EXAMPLES=("storage_example", "simple_reservoir_and_pipe_example", "watershed_calibration")
 EXTS=["inp", "inp_", "yaml", "cal"]
 EXTS.extend([x.upper() for x in EXTS])
 EXAMPLES=list(product(EXAMPLES,EXTS))
 package_data=[ "examples/"+x[0]+"/*."+x[1] for x in EXAMPLES]
 #package_data.append("*.pyw")
+LONGDISC=sc.LONGDISC+"""
+\n--------\nREADME.txt\n-------
+%(rm)s\n""" % { "rm": README}
 setup(
     name = sc.NAME,
     packages = ["swmm5ea"],
@@ -23,5 +29,5 @@ setup(
     download_url = sc.DLURL,
     keywords = ["EPA-SWMM", "GA", "Urban Drainage"],
     classifiers = sc.CLASSIFY,
-    long_description = sc.LONGDISC
+    long_description = LONGDISC
 )
